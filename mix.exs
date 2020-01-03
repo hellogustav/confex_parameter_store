3d3a58ee189d09231bb0caa48bd3ccc709d8fa23
Kamil Bielawski <k_bielawski@fastmail.fm>
2020-01-03 11:26:23 +0100

    Bump confex version in deps

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule ConfexParameterStore.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:confex, "~> 3.3.0"},
+      {:confex, "~> 3.4.0"},
       {:ex_aws_ssm, github: "hellogustav/ex_aws_ssm"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
